9e612ca2857a676a6628f6afa3b11c2e836570bd
Isaias RoblesVega <isak4567@gmail.com>
2023-05-13 20:24:42 -0300

    Conexiones y funcion de muestra de comentario

--- scripts/Comentarios.js
+++ scripts/Comentarios.js
@@ -1,6 +1,19 @@
 import { getFirestore, collection, getDocs, addDoc, doc, deleteDoc } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js'
 import { db } from './firebaseConfig.js';
 
+
+const comForm = document.querySelector('.ContactoForm');
+let formComNombre = comForm.querySelector('#formComNombre');
+
+comForm.addEventListener('submit', (e) => {
+
+        e.preventDefault();
+        formComNombre = comForm.querySelector('#formComNombre');
+
+        console.log(formComNombre.value);
+});
+
+
         // agrega datos a la coleccion
 /*         try {
         const docRef = await addDoc(collection(db, "Comentarios"), {
--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,18 @@
 import { collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js';
 import { db } from './firebaseConfig.js';
 
-// Video
-
-
 
 // Muestra de algunos comentarios
 // Mostrar
+let indexCom = document.querySelector('#indexCom');
+
 const querySnapshot = await getDocs(collection(db, "Comentarios"));
 querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data().persona}`);
+
+    indexCom.innerHTML += `<div class="referencia">
+                    <p class="nombreRef">${doc.data().persona}</p>
+                    <p class="comRef">${doc.data().comentario}</p>
+                </div>`
+
+    //console.log(`${doc.id} => ${doc.data().persona}`);
 });

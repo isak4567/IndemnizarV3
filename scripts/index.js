7acdbde69227e8c30f9c35363848358bdbeadf4a
Isaias RoblesVega <isak4567@gmail.com>
2023-05-23 13:39:40 -0300

    index resposive 360 to 540 clear

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,10 @@ querySnapshot.forEach((doc) => {
 
     //console.log(`${doc.id} => ${doc.data().persona}`);
 });
+
+const hamMenu = document.querySelector('.hamMenu');
+const nav = document.querySelector('nav ul');
+
+hamMenu.addEventListener('click', ()=> {
+    nav.classList.toggle('disflex');
+});
